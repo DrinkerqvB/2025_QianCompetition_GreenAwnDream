947e9635bef1b09e410d2dc5de4bd221e344d711
Skylark2006 <3978307331@qq.com>
2025-06-08 20:17:30 +0800

    第一版修改（不确定传上去的情况）
    第一版修改（不确定传上去的情况）

---
+++ 2025_QianCompetition_GreenAwnDream-develop test(1)/HARDWARE/encoder.c
@@ -0,0 +1,284 @@
+#include "encoder.h"
+
+/**************************************************************************
+Function: Initialize TIM2 as the encoder interface mode
+Input   : none
+Output  : none
+函数功能：把TIM2初始化为编码器接口模式
+入口参数：无
+返 回 值：无
+**************************************************************************/
+void Encoder_Init_TIM2(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+	TIM_ICInitTypeDef TIM_ICInitStructure;  
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);   //使能定时器
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA|RCC_AHB1Periph_GPIOB, ENABLE);  //使用A B口
+ 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;  //PA15
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);  
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;  //PB3
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);  
+
+  GPIO_PinAFConfig(GPIOA,GPIO_PinSource15,GPIO_AF_TIM2);   //复用为TIM2 编码器接口
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource3,GPIO_AF_TIM2);   //复用为TIM2 编码器接口
+  
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; 							// No prescaling     //不分频
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD;  //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; //TIM向上计数    
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);  //初始化定时器
+  
+  TIM_EncoderInterfaceConfig(TIM2, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+  TIM_ICStructInit(&TIM_ICInitStructure);
+  TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM2, &TIM_ICInitStructure);
+  
+  TIM_ClearFlag(TIM2, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+  TIM_SetCounter(TIM2,0);
+  TIM_Cmd(TIM2, ENABLE); 
+}
+
+/**************************************************************************
+Function: Initialize TIM3 as the encoder interface mode
+Input   : none
+Output  : none
+函数功能：把TIM3初始化为编码器接口模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Init_TIM3(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_ICInitTypeDef TIM_ICInitStructure;  
+  GPIO_InitTypeDef GPIO_InitStructure;
+	
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);   //使能定时器
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);  //使用A口
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;  //PB4 PB5
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);  
+
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource4,GPIO_AF_TIM3);   //复用为TIM2 编码器接口
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource5,GPIO_AF_TIM3);   //复用为TIM2 编码器接口
+  
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);  
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x0; 							// No prescaling     //不分频
+  TIM_TimeBaseStructure.TIM_Period = ENCODER_TIM_PERIOD;  //设定计数器自动重装值
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //选择时钟分频：不分频
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; //TIM向上计数    
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);  //初始化定时器
+  
+  TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);//使用编码器模式3
+  TIM_ICStructInit(&TIM_ICInitStructure);
+  TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM3, &TIM_ICInitStructure);  
+	
+  TIM_ClearFlag(TIM3, TIM_FLAG_Update);//清除TIM的更新标志位
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+	TIM_SetCounter(TIM3,0);
+  TIM_Cmd(TIM3, ENABLE); 
+}
+
+
+
+
+/**************************************************************************
+函数功能：把PA2、PB0初始化为外部中断模式
+入口参数：无
+返回  值：无
+**************************************************************************/
+void Encoder_Z_Init(void)
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+	
+	//PA2
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_DOWN;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	//PB0
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_DOWN;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA,EXTI_PinSource2);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB,EXTI_PinSource0);
+	
+	EXTI_InitTypeDef EXTI_InitStructure;
+	EXTI_InitStructure.EXTI_Line=EXTI_Line2;
+	EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;
+	EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Rising;
+	EXTI_InitStructure.EXTI_LineCmd=ENABLE;
+	EXTI_Init(&EXTI_InitStructure);
+	
+	EXTI_InitStructure.EXTI_Line=EXTI_Line0;
+	EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;
+	EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Rising;
+	EXTI_InitStructure.EXTI_LineCmd=ENABLE;
+	EXTI_Init(&EXTI_InitStructure);
+
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel=EXTI2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	NVIC_InitStructure.NVIC_IRQChannel=EXTI0_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+	NVIC_Init(&NVIC_InitStructure);
+	
+}
+
+
+
+
+/**************************************************************************
+函数功能：PA2,PB0中断函数
+入口参数：无
+返回  值：无
+**************************************************************************/
+void EXTI0_IRQHandler(void)
+{
+	if(EXTI_GetITStatus(EXTI_Line0)==SET){
+		
+		TIM_SetCounter(TIM3,0x00);
+		
+		EXTI_ClearITPendingBit(EXTI_Line0);
+	}
+}
+
+
+void EXTI2_IRQHandler(void)
+{
+	if(EXTI_GetITStatus(EXTI_Line2)==SET){
+		
+		TIM_SetCounter(TIM2,0x00);
+		
+		EXTI_ClearITPendingBit(EXTI_Line2);
+	}
+}
+
+/**************************************************************************
+Function: Read the encoder count
+Input   : The timer
+Output  : Encoder value (representing speed)
+函数功能：读取编码器计数
+入口参数：定时器（一个数字）
+返回  值：编码器数值(代表速度)
+**************************************************************************/
+int Read_Encoder(u8 TIMX)
+{
+ int Encoder_TIM;    
+ switch(TIMX)
+ {
+	case 2:  Encoder_TIM= (short)TIM2 -> CNT;   TIM2 -> CNT=0;  break;
+	case 3:  Encoder_TIM= (short)TIM3 -> CNT;   TIM3 -> CNT=0;  break;
+	//case 4:  Encoder_TIM= (short)TIM4 -> CNT;   TIM4 -> CNT=0;  break;	
+	//case 5:  Encoder_TIM= (short)TIM5 -> CNT;   TIM5 -> CNT=0;  break;	
+	default: Encoder_TIM=0;
+ }
+	return Encoder_TIM;
+}
+
+/**************************************************************************
+Function: Tim2 interrupt service function
+Input   : none
+Output  : none
+函数功能：TIM2中断服务函数
+入口参数：无
+返 回 值：无
+**************************************************************************/
+void TIM2_IRQHandler(void)
+{ 		    		  			    
+	if(TIM2->SR&0X0001) //Overflow interrupt //溢出中断
+	{    				   				     	    	
+	}				   
+	TIM2->SR&=~(1<<0); //Clear the interrupt flag bit //清除中断标志位 	    
+}
+/**************************************************************************
+Function: Tim3 interrupt service function
+Input   : none
+Output  : none
+函数功能：TIM3中断服务函数
+入口参数：无
+返 回 值：无
+**************************************************************************/
+void TIM3_IRQHandler(void)
+{ 		    		  			    
+	if(TIM3->SR&0X0001) //Overflow interrupt //溢出中断
+	{    				   				     	    	
+	}				   
+	TIM3->SR&=~(1<<0); //Clear the interrupt flag bit //清除中断标志位  	    
+}
+///**************************************************************************
+//Function: Tim4 interrupt service function
+//Input   : none
+//Output  : none
+//函数功能：TIM4中断服务函数
+//入口参数：无
+//返 回 值：无
+//**************************************************************************/
+//void TIM4_IRQHandler(void)
+//{ 		    		  			    
+//	if(TIM4->SR&0X0001) //Overflow interrupt //溢出中断
+//	{    				   				     	    	
+//	}				   
+//	TIM4->SR&=~(1<<0); //Clear the interrupt flag bit //清除中断标志位  	    
+//}
+
+///**************************************************************************
+//Function: Tim9 interrupt service function
+//Input   : none
+//Output  : none
+//函数功能：TIM9中断服务函数
+//入口参数：无
+//返 回 值：无
+//**************************************************************************/
+//void TIM5_IRQHandler(void)
+//{ 		    		  			    
+//	if(TIM5->SR&0X0001) //Overflow interrupt //溢出中断
+//	{    				   				     	    	
+//	}				   
+//	TIM5->SR&=~(1<<0); //Clear the interrupt flag bit //清除中断标志位  	    
+//}
+
+//void TIM8_BRK_TIM12_IRQHandler(void)
+//{
+//	if(TIM12->SR&0X0001) //Overflow interrupt //溢出中断
+//	{    				   				     	    	
+//	}				   
+//	TIM12->SR&=~(1<<0); //Clear the interrupt flag bit //清除中断标志位  	 
+
+//}
